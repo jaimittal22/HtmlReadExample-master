bd99a01ac7b964274d763a7861c6859af4e06f09
jaimittal <jai_mittal24@milton.edu>
2023-12-13 11:31:58 -0500

    Clean links

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -22,7 +22,30 @@ public class HtmlRead {
                 if(line.contains("href=")) {
                     line.indexOf("href=");
                     int abcd = line.indexOf("href=");
-                    System.out.println(line.substring(abcd+6));
+
+                String pqrs = line.substring(abcd+6);
+//                System.out.println("og"+pqrs);
+                pqrs.indexOf("\"");
+                int end = pqrs.indexOf("\"");
+                pqrs.indexOf("'");
+                int end1 = pqrs.indexOf("'");
+//               System.out.println(end);
+//         System.out.print(end1);
+                    if(end1 <0){
+                        String link = pqrs.substring(0,end);
+                        System.out.println(link);
+                    } else if (end <0) {
+                        String link = pqrs.substring(0,end1);
+                        System.out.println(link);
+                    }
+                    else if(end<end1){
+                        String link = pqrs.substring(0,end);
+                        System.out.println(link);
+                    }else{
+                        String link = pqrs.substring(0,end1);
+                        System.out.println(link);
+                    }
+                 //   System.out.println(pqrs.substring(0,end));
                 }
             }
             reader.close();
